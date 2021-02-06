c83e07296f825c706fb48475bac6ffc6a1cf8140
kinattaporn <kinattaporn@gmail.com>
2021-02-06 15:40:31 -0800

    Update MinTemperatures to add Max

--- src/main/scala/com/sundogsoftware/spark/MinTemperatures.scala
+++ src/main/scala/com/sundogsoftware/spark/MinTemperatures.scala
@@ -3,6 +3,7 @@ package com.sundogsoftware.spark
 import org.apache.spark._
 import org.apache.log4j._
 import scala.math.min
+import scala.math.max
 
 /** Find the minimum temperature by weather station */
 object MinTemperatures {
@@ -28,25 +29,50 @@ object MinTemperatures {
     
     // Convert to (stationID, entryType, temperature) tuples
     val parsedLines = lines.map(parseLine)
+    println("--------------------------- parsedLines")
+    parsedLines.take(5).foreach(println)
     
     // Filter out all but TMIN entries
     val minTemps = parsedLines.filter(x => x._2 == "TMIN")
+    println("--------------------------- minTemps")
+    minTemps.take(5).foreach(println)
     
     // Convert to (stationID, temperature)
     val stationTemps = minTemps.map(x => (x._1, x._3.toFloat))
+    println("--------------------------- stationTemps")
+    stationTemps.take(5).foreach(println)
+
+    // Group By stationID
+    val tempsByStation = stationTemps.groupByKey()
+    println("--------------------------- tempsByStation")
+    tempsByStation.take(5).foreach(println)
     
     // Reduce by stationID retaining the minimum temperature found
-    val minTempsByStation = stationTemps.reduceByKey( (x,y) => min(x,y))
+    val minTempsByStation = stationTemps.reduceByKey((x,y) => min(x,y))
+    println("--------------------------- minTempsByStation")
+    minTempsByStation.collect().foreach(println)
+
+    // Reduce by stationID retaining the maximum temperature found
+    val maxTempsByStation = stationTemps.reduceByKey((x,y) => max(x,y))
+    println("--------------------------- maxTempsByStation")
+    maxTempsByStation.collect().foreach(println)
     
     // Collect, format, and print the results
     val results = minTempsByStation.collect()
-    
+    println("--------------------------- results")
     for (result <- results.sorted) {
        val station = result._1
        val temp = result._2
        val formattedTemp = f"$temp%.2f F"
        println(s"$station minimum temperature: $formattedTemp") 
     }
-      
+    val resultsMax = maxTempsByStation.collect()
+    println("--------------------------- resultsMax")
+    for (result <- resultsMax.sorted) {
+      val station = result._1
+      val temp = result._2
+      val formattedTemp = f"$temp%.2f F"
+      println(s"$station minimum temperature: $formattedTemp")
+    }
   }
 }
